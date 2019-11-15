1da89851e3fe84893058f807d938a9701d59bd53
Sergey V. Krupov <sergey.krupov@gmail.com>
2019-11-15 10:44:35 +0500

    CoreData

---
+++ NewsReader/Storage/ContentDownloadManager.swift
@@ -0,0 +1,46 @@
+//
+//  ContentDownloadManager.swift
+//  NewsReader
+//
+//  Created by Sergey V. Krupov on 15.11.2019.
+//  Copyright © 2019 Sergey V. Krupov. All rights reserved.
+//
+
+import CoreData
+
+protocol ContentDownloadManager {
+
+}
+
+// TODO: Notification
+
+final class ContentDownloadManagerImpl: ContentDownloadManager {
+
+    init(container: NSPersistentContainer, service: NewsApiService) {
+        self.container = container
+        self.service = service
+    }
+
+    // FIXME: возможность нескольких запросов
+    func search(query: String, completion: @escaping (Error?) -> Void) {
+        let request = EverythingRequest(request: query)
+        service.requestEverything(request) { [container] result in
+            do {
+                let response = try result.get()
+                container.performBackgroundTask { context in
+                    response.
+                }
+            } catch {
+                completion(error)
+            }
+        }
+    }
+
+    func loadNextPage(completion: @escaping (Error?) -> Void) {
+
+    }
+
+    // MARK: - Private
+    private let container: NSPersistentContainer
+    private let service: NewsApiService
+}
